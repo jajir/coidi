d5f5b2360ad1c8c2abacfdc8a71eb06c5ea567d3
"jan" <"jan"@12bfccd7-1be7-4603-8301-146c0ccc8fc6>
2012-06-16 17:44:48 +0000

    improves XRDS document

--- trunk/op/src/main/java/com/coroptis/coidi/op/services/impl/XrdsServiceImpl.java
+++ trunk/op/src/main/java/com/coroptis/coidi/op/services/impl/XrdsServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.slf4j.Logger;
 
 import com.coroptis.coidi.CoidiException;
 import com.coroptis.coidi.core.services.ConfigurationService;
@@ -13,8 +14,11 @@ import com.google.common.base.Preconditions;
 public class XrdsServiceImpl implements XrdsService {
 
 	@Inject
+	private Logger logger;
+
+	@Inject
 	private ConfigurationService configurationService;
-	
+
 	@Inject
 	@Symbol("op.server")
 	private String server;
@@ -28,7 +32,7 @@ public class XrdsServiceImpl implements XrdsService {
 		buff.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 		buff.append("<xrds:XRDS xmlns:xrds=\"xri://$xrds\" xmlns=\"xri://$xrd*($v*2.0)\" xmlns:openid=\"http://openid.net/xmlns/1.0\">\n");
 		buff.append("  <XRD>\n");
-		buff.append("    <Service xmlns=\"xri://$xrd*($v*2.0)\">\n");
+		buff.append("    <Service priority=\"100\" xmlns=\"xri://$xrd*($v*2.0)\">\n");
 		buff.append("      <Type>http://specs.openid.net/auth/2.0/signon</Type>\n");
 		buff.append("      <URI>");
 		buff.append(conf.get("server"));
@@ -39,8 +43,15 @@ public class XrdsServiceImpl implements XrdsService {
 		buff.append(user);
 		buff.append("</LocalID>\n");
 		buff.append("    </Service>\n");
+		buff.append("    <Service priority=\"10\" xmlns=\"xri://$xrd*($v*2.0)\">\n");
+		buff.append("      <Type>http://specs.openid.net/auth/2.0/server</Type>\n");
+		buff.append("      <URI>");
+		buff.append(conf.get("server"));
+		buff.append("openid</URI>\n");
+		buff.append("    </Service>\n");
 		buff.append("  </XRD>\n");
 		buff.append("</xrds:XRDS>");
+		logger.debug("returning XRDS document: " + buff.toString());
 		return buff.toString();
 	}
 
