f4c078c8bf86b00841e123d620a3d094f06882b4
"jan" <"jan"@12bfccd7-1be7-4603-8301-146c0ccc8fc6>
2012-07-03 19:19:50 +0000

    added some junit tests

--- trunk/rp/src/test/java/com/coroptis/coidi/rp/integration/ProcessAuthResponseTest.java
+++ trunk/rp/src/test/java/com/coroptis/coidi/rp/integration/ProcessAuthResponseTest.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import com.coroptis.coidi.core.message.AuthenticationResponse;
 import com.coroptis.coidi.core.services.MessageService;
 import com.coroptis.coidi.rp.base.AuthRespExtension;
 import com.coroptis.coidi.rp.base.XrdService;
@@ -15,7 +16,8 @@ public class ProcessAuthResponseTest extends AbstractIntegrationTest {
 	private final Logger logger = Logger
 			.getLogger(ProcessAuthResponseTest.class);
 
-	private String msg = "openid.ns=http://specs.openid.net/auth/2.0&openid.mode=id_res&openid.op_endpoint=https://www.google.com/accounts/o8/ud&openid.response_nonce=2012-07-01T22:51:51ZOtF5fXQpmVvSeQ&openid.return_to=http://www.trainindex.com/userProfile&openid.assoc_handle=AMlYA9XgrO-etZh-FMZ7tYvZk5EHe_pDVEEmWjyzi-AJy7m9FYDE1eMN&openid.signed=op_endpoint,claimed_id,identity,return_to,response_nonce,assoc_handle,ns.ext1,ext1.mode,ext1.type.firstname,ext1.value.firstname,ext1.type.email,ext1.value.email&openid.sig=VvM3WsuezKL5XJ1cQLpr9ERc7Os=&openid.identity=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.claimed_id=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.ns.ext1=http://openid.net/srv/ax/1.0&openid.ext1.mode=fetch_response&openid.ext1.type.firstname=http://axschema.org/namePerson/first&openid.ext1.value.firstname=Jan&openid.ext1.type.email=http://axschema.org/contact/email&openid.ext1.value.email=jirout@gmail.com";
+	private String msg = "openid.ns=http://specs.openid.net/auth/2.0&openid.mode=id_res&openid.op_endpoint=https://www.google.com/accounts/o8/ud&openid.response_nonce=2012-07-01T22:51:51ZOtF5fXQpmVvSeQ&openid.return_to=http://www.trainindex.com/userProfile&openid.assoc_handle=AMlYA9XgrO-etZh-FMZ7tYvZk5EHe_pDVEEmWjyzi-AJy7m9FYDE1eMN&openid.signed=op_endpoint,claimed_id,identity,return_to,response_nonce,assoc_handle,ns.ext1,ext1.mode,ext1.type.firstname,ext1.value.firstname,ext1.type.email,ext1.value.email&openid.sig=VvM3WsuezKL5XJ1cQLpr9ERc7Os=&openid.identity=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.claimed_id=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.ns.ext1=http://openid.net/srv/ax/1.0&openid.ext1.mode=fetch_response&openid.ext1.type.firstname=http://axschema.org/namePerson/first&openid.ext1.value.firstname=Kotoucek&openid.ext1.type.email=http://axschema.org/contact/email&openid.ext1.value.email=kachna@gmail.com";
+	private String msg2 = "openid.ns=http://specs.openid.net/auth/2.0&openid.mode=id_res&openid.op_endpoint=https://www.google.com/accounts/o8/ud&openid.response_nonce=2012-07-01T22:51:51ZOtF5fXQpmVvSeQ&openid.return_to=http://www.trainindex.com/userProfile&openid.assoc_handle=AMlYA9XgrO-etZh-FMZ7tYvZk5EHe_pDVEEmWjyzi-AJy7m9FYDE1eMN&openid.signed=op_endpoint,claimed_id,identity,return_to,response_nonce,assoc_handle,ns.ext1,ext1.mode,ext1.type.firstname,ext1.value.firstname,ext1.type.email,ext1.value.email&openid.sig=VvM3WsuezKL5XJ1cQLpr9ERc7Os=&openid.identity=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.claimed_id=https://www.google.com/accounts/o8/id?id=AItOawlWAYnSZs5g1Lp6vMZSnK_0Giy7tyRttCI&openid.ns.ext1=http://openid.net/srv/ax/1.0&openid.ext1.mode=fetch_response&openid.ext1.type.firstname=http://axschema.org/namePerson/first&openid.ext1.value.firstname=Kotoucek&openid.ext1.type.email=http://axschema.org/contact/email&openid.ext1.value.email=kachna@gmail.com";
 
 	public void testProcess() throws Exception {
 		MessageService messageService = getService(MessageService.class);
@@ -27,6 +29,22 @@ public class ProcessAuthResponseTest extends AbstractIntegrationTest {
 		AuthRespExtension ex = resp.get(XrdService.TYPE_ATTRIBUTE_EXCHANGE_1_0);
 		assertNotNull(ex);
 		logger.debug(ex.getValueForName("email"));
+		assertEquals("kachna@gmail.com", ex.getValueForName("email"));
+		assertEquals("Kotoucek", ex.getValueForName("firstname"));
+	}
+
+	public void testProcess2() throws Exception {
+		MessageService messageService = getService(MessageService.class);
+		AuthenticationService authenticationService = getService(AuthenticationService.class);
+		Map<String, String> map = messageService.convertUrlToMap(msg2);
+		Map<String, AuthRespExtension> resp = authenticationService
+				.generateResponse(new AuthenticationResponse(map));
+		logger.debug(resp.size());
+		AuthRespExtension ex = resp.get(XrdService.TYPE_ATTRIBUTE_EXCHANGE_1_0);
+		assertNotNull(ex);
+		logger.debug(ex.getValueForName("email"));
+		assertEquals("kachna@gmail.com", ex.getValueForName("email"));
+		assertEquals("Kotoucek", ex.getValueForName("firstname"));
 	}
 
 }
