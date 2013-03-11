5896cfc54a8beab0b297494bf12c043299fccd60
"jan" <"jan"@12bfccd7-1be7-4603-8301-146c0ccc8fc6>
2013-03-11 21:24:58 +0000

    added identities viewing

--- coidi-dist/trunk/coidi-test-op/src/main/java/com/coroptis/coidi/op/view/services/OpViewModule.java
+++ coidi-dist/trunk/coidi-test-op/src/main/java/com/coroptis/coidi/op/view/services/OpViewModule.java
@@ -15,6 +15,7 @@
  */
 package com.coroptis.coidi.op.view.services;
 
+import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
@@ -22,24 +23,23 @@ import org.apache.tapestry5.ioc.annotations.InjectService;
 import org.apache.tapestry5.services.Dispatcher;
 
 import com.coroptis.coidi.op.view.services.impl.AccessControllerDispatcher;
+import com.coroptis.coidi.op.view.services.impl.IdentityGds;
 
 public class OpViewModule {// NO_UCD
 
-	public static void bind(ServiceBinder binder) {
-		binder.bind(Dispatcher.class, AccessControllerDispatcher.class).withId(
-				"accessControllerDispatcher");
-	}
+    public static void bind(ServiceBinder binder) {
+	binder.bind(Dispatcher.class, AccessControllerDispatcher.class).withId(
+		"accessControllerDispatcher");
+	binder.bind(GridDataSource.class, IdentityGds.class).withId("identityGds");
+    }
 
-	public static void contributeMasterDispatcher(
-			OrderedConfiguration<Dispatcher> configuration,
-			@InjectService("accessControllerDispatcher") Dispatcher accessController) {
-		configuration.add("accessControllerDispatcher", accessController,
-				"before:PageRender");
-	}
+    public static void contributeMasterDispatcher(OrderedConfiguration<Dispatcher> configuration,
+	    @InjectService("accessControllerDispatcher") Dispatcher accessController) {
+	configuration.add("accessControllerDispatcher", accessController, "before:PageRender");
+    }
 
-	public static void contributeHibernateEntityPackageManager(
-			Configuration<String> conf) {
-		conf.add("com.coroptis.coidi.op.entities");
-	}
+    public static void contributeHibernateEntityPackageManager(Configuration<String> conf) {
+	conf.add("com.coroptis.coidi.op.entities");
+    }
 
 }
