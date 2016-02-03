9a2860e6e66d43147deb4e35bbf4e026f8938f8e
jiroutj <Jan.Jirout@equabank.cz>
2016-02-03 18:51:19 +0100

    AuthenticationProcvesor should not verify user's identity, log4j conf was moved to test resources

--- coidi-core/src/main/java/com/coroptis/coidi/op/services/impl/AuthProcVerifyIdentity20.java
+++ coidi-core/src/main/java/com/coroptis/coidi/op/services/impl/AuthProcVerifyIdentity20.java
@@ -57,12 +57,12 @@ public class AuthProcVerifyIdentity20 implements AuthenticationProcessor {
 	    return negativeResponseGenerator.simpleError("User is not logged at OP");
 	}
 
-	if (!StringUtils.isEmpty(authenticationRequest.getIdentity())) {
-	    if (!openIdRequestTool.verify(authenticationRequest.getIdentity(), userSession)) {
-		return negativeResponseGenerator.simpleError("Requested identity '"
-			+ authenticationRequest.getIdentity() + "' doesn't exists.");
-	    }
-	}
+//	if (!StringUtils.isEmpty(authenticationRequest.getIdentity())) {
+//	    if (!openIdRequestTool.verify(authenticationRequest.getIdentity(), userSession)) {
+//		return negativeResponseGenerator.simpleError("Requested identity '"
+//			+ authenticationRequest.getIdentity() + "' doesn't exists.");
+//	    }
+//	}
 	return null;
 
     }
