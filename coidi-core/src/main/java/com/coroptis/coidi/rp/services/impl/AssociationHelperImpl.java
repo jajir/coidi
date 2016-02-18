77e25222ab33c16dabbb910455d3d731abdfbfb7
Jan Jirout <jirout@gmail.com>
2016-02-18 20:46:10 +0100

    added snapshot version and missing @override annotation

--- coidi-core/src/main/java/com/coroptis/coidi/rp/services/impl/AssociationHelperImpl.java
+++ coidi-core/src/main/java/com/coroptis/coidi/rp/services/impl/AssociationHelperImpl.java
@@ -8,29 +8,32 @@ import com.google.common.base.Strings;
 
 public class AssociationHelperImpl implements AssociationHelper {
 
-	@Override
-	public void verify(final AssociationResponse associationResponse) {
-		checkMandatoryField(associationResponse, "assoc_handle");
-		checkMandatoryField(associationResponse, "dh_server_public");
-		checkMandatoryField(associationResponse, "session_type");
-		checkMandatoryField(associationResponse, "expires_in");
-		checkMandatoryField(associationResponse, "enc_mac_key");
-		checkMandatoryField(associationResponse, "assoc_type");
-	}
+    @Override
+    public void verify(final AssociationResponse associationResponse) {
+	checkMandatoryField(associationResponse, "assoc_handle");
+	checkMandatoryField(associationResponse, "dh_server_public");
+	checkMandatoryField(associationResponse, "session_type");
+	checkMandatoryField(associationResponse, "expires_in");
+	checkMandatoryField(associationResponse, "enc_mac_key");
+	checkMandatoryField(associationResponse, "assoc_type");
+    }
 
-	public void verifyNameSpace20(final AssociationResponse message) {
-		if (Strings.isNullOrEmpty(message.getNameSpace())) {
-			throw new CoidiException("OpenID namespace was not filled.");
-		}
-		if (!AbstractMessage.OPENID_NS_20.equals(message.getNameSpace())) {
-			throw new CoidiException("OpenID namespace contains invalid value '" + message.getNameSpace() + "'.");
-		}
+    @Override
+    public void verifyNameSpace20(final AssociationResponse message) {
+	if (Strings.isNullOrEmpty(message.getNameSpace())) {
+	    throw new CoidiException("OpenID namespace was not filled.");
 	}
+	if (!AbstractMessage.OPENID_NS_20.equals(message.getNameSpace())) {
+	    throw new CoidiException(
+		    "OpenID namespace contains invalid value '" + message.getNameSpace() + "'.");
+	}
+    }
 
-	private void checkMandatoryField(final AssociationResponse associationResponse, final String field) {
-		if (Strings.isNullOrEmpty(associationResponse.get(field))) {
-			new CoidiException("Mandatory field '" + field + "' is empty");
-		}
+    private void checkMandatoryField(final AssociationResponse associationResponse,
+	    final String field) {
+	if (Strings.isNullOrEmpty(associationResponse.get(field))) {
+	    new CoidiException("Mandatory field '" + field + "' is empty");
 	}
+    }
 
 }
